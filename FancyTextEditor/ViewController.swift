db8db248401a1b316d15e10f70863e2f653645f2
Sujanth <sujanth1987@gmail.com>
2017-08-01 21:25:23 +1000

    UI completed

--- FancyTextEditor/ViewController.swift
+++ FancyTextEditor/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textFieldPressed(_ sender: Any) {
+        label.text = textField.text
+        self.resignFirstResponder()
+    }
+    
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func fontButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func shadowButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func sizeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
